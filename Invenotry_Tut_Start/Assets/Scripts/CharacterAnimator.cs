2cdd80279bcf5ac66639d63bb5ca60437f002c07
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2024-04-06 17:02:27 +0300

    Replace blend files with fbx; Some changes

--- Invenotry_Tut_Start/Assets/Scripts/CharacterAnimator.cs
+++ Invenotry_Tut_Start/Assets/Scripts/CharacterAnimator.cs
@@ -7,8 +7,8 @@ public class CharacterAnimator : MonoBehaviour {
 
     const float locomationAnimationSmoothTime = .1f;
 
-    NavMeshAgent agent;
-    Animator animator;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator animator;
 
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -16,7 +16,7 @@ public class CharacterAnimator : MonoBehaviour {
 	}
 	
 	void Update () {
-        float speedPercent = agent.velocity.magnitude / agent.speed;
-        animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
+        //float speedPercent = agent.velocity.magnitude / agent.speed;
+        //animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
 	}
 }
